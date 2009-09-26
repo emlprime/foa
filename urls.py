aefdf23e8c9e4040c5207b3871b96712d2ede22f
Laura Madsen <lauramadsen@yt.local>
2009-09-25 17:21:58 -0700

    changed font to apple chancery, followed by arial.  commented out object detail url for the time being.

--- urls.py
+++ urls.py
@@ -24,8 +24,8 @@ urlpatterns += patterns ('django.views.generic.list_detail',
     (r'^products/$', 'object_list', {'queryset':Product.objects.all(),'template_name':'products.html'}, "products"),
 )
 
-
+"""
 urlpatterns += patterns ('django.views.generic.list_detail.object_detail',
     (r'^products/detail/$', {'queryset':Product.objects.all(), 'object_id':Product.objects.get(id=1).id, 'template_name':'product_detail.html', 'template_object_name':'product'}),
 )
-
+"""
