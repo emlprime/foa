cbed28234e164cededc99c112321f5e739ffb92a
Create a User <foa@emlprime.com>
2009-10-08 16:38:21 +0000

    added xml sitemap

--- urls.py
+++ urls.py
@@ -17,6 +17,7 @@ urlpatterns = patterns('',
 
 urlpatterns += patterns('django.views.generic.simple',
     (r'^$', 'direct_to_template', {'template':'index.html'}, "index"),
+    (r'^sitemap.xml$', 'direct_to_template', {'template':'sitemap.xml'}, "sitemap"),
     (r'^about/$', 'direct_to_template', {'template':'about.html'}, "about"),
     (r'^products/cancel/$', 'direct_to_template', {'template':'cancel.html'}, "about"),
     (r'^products/thanks/$', 'direct_to_template', {'template':'thanks.html'}, "about"),
