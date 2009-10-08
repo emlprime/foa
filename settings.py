0bda0512d8efab4298df129d9f24fdb980fc63ef
Create a User <foa@emlprime.com>
2009-10-08 05:31:20 +0000

    changed the settings to work with admin media

--- settings.py
+++ settings.py
@@ -51,7 +51,7 @@ MEDIA_URL = '/site_media/'
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/media/admin/'
+ADMIN_MEDIA_PREFIX = '/admin_media/'
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = '-af7+y29o18a1-ele8$t5raxcrjcgl3i7j5#n5+2s4bci3z=w1'
